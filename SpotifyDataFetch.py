06f746e853bf2043c641747647778a0820ec9e6f
PranavNarayan7034 <pranav@expertzlab.com>
2025-12-09 21:37:50 +0530

    first commit

---
+++ SpotifyDataFetch.py
@@ -0,0 +1,58 @@
+
+import base64
+import requests
+import json
+from dotenv import load_dotenv
+
+import os 
+load_dotenv('.env_local')
+
+# Get the values
+CLIENT_ID = os.getenv('CLIENT_ID')
+CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+
+# creating a token for spotify verified
+def access_token():
+    try:
+        credentials = f"{CLIENT_ID}:{CLIENT_SECRET}"
+        encoded_credentials = base64.b64encode(credentials.encode()).decode()
+        response = requests.post(
+            'https://accounts.spotify.com/api/token',
+            headers={'Authorization': f'Basic {encoded_credentials}'},
+            data={'grant_type': 'client_credentials'})
+
+        return response.json()['access_token']
+    except Exception as e:
+        print("Error in Token Generation..", e)
+
+
+# Latest release in spotify
+def get_new_release():
+    try:
+        token = access_token()
+        header = {'Authorization': f'Bearer {token}'}
+        Param = {'limit': 50}
+        response = requests.get('https://api.spotify.com/v1/browse/new-releases',
+                                headers=header, params=Param)
+        if response.status_code == 200:
+            data = response.json()
+            albums = data['albums']['items']
+
+            # Extract only important information
+            for album in albums:
+                important_info = {
+                    'album_name': album['name'],
+                    'artist_name': album['artists'][0]['name'],
+                    'release_date': album['release_date'],
+                    'album_type': album['album_type'],
+                    'total_tracks': album['total_tracks'],
+                    'spotify_url': album['external_urls']['spotify'],
+                    'album_image': album['images'][0]['url'] if album['images'] else None
+                }
+                print(json.dumps(important_info, indent=2))
+                print("-" * 80)
+    except Exception as e:
+        print("Error in latest release data fetching ..", e)
+
+
+get_new_release()
--- main.py
+++
@@ -1,8 +0,0 @@
-import numpy as np  
-
-b = np.array([1])
-print(b)
-
-
-a = 10+52000
-print(a)
